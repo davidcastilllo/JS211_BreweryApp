8b99ad35168d64cffcd63913bccb91712d2414ab
David Castillo <seth.castillo4@gmail.com>
2021-11-03 19:19:58 -0500

    Updated Project

--- main.js
+++ main.js
@@ -21,8 +21,21 @@ let displayBrew = (breweries) => {
   let li = document.createElement('li')
   li.classList = "brewList"
   li.innerHTML = `<b>Name:</b> ${b.name} <br> <b>Address:</b> ${b.street} <br> <b>Phone:</b> ${b.phone} <br> <b>Website:</b> ${b.website_url}`
+  let heart = document.createElement('img')
+  heart.classList = 'button'
+  heart.src = "./Star.png" 
+  heart.onclick = (e) => {favorite(e)}
+  li.appendChild(heart)
   ul.appendChild(li)
  });
  document.getElementById('list').appendChild(ul)
 }
 
+let favorite = (e) => {
+ if (e.target.src == "http://127.0.0.1:5500/Star.png") {
+  e.target.src = './Starred.png'
+ }
+ else {
+  e.target.src = "./Star.png" 
+ }
+}
